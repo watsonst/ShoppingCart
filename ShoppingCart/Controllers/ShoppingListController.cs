c4b4dfa18ee3ee502315cf693a3c5c42754ee5b5
Stephanie <me@stephaniemariewatson.com>
2021-10-27 20:21:29 -0400

    Add project files.

---
+++ ShoppingCart/Controllers/ShoppingListController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingCart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Controllers
+{
+   public class TempCartData
+    {
+        public static List<CartItems> Items = new List<CartItems>()
+        {
+               new CartItems() {Id = 1, Name = "Pasta", Type = "Food"} //By putting it in a static it will be there the whole process. but once we restart the application will be gone.
+        };
+    }
+    
+   
+    
+    
+    public class ShoppingListController : Controller
+    {
+
+        private List<CartItems> _allItems; // allitems list here only lives in the controllor. won't store, wont be available for the life of the MVC process
+
+
+
+        public ShoppingListController()
+        {
+            _allItems = TempCartData.Items;
+        }
+
+        // GET: ShoppingListController
+        public ActionResult Index()
+        {
+            ViewBag.Name = "Paul";
+            ViewData["Message"] = "here is the shopping cart: ";
+            return View(_allItems);
+        }
+
+        // GET: ShoppingListController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ShoppingListController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ShoppingListController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind("Id", "Name", "Type")]CartItems newItem)
+        {
+            try
+            {
+                TempCartData.Items.Add(newItem);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ShoppingListController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ShoppingListController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ShoppingListController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ShoppingListController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ShoppingCart/Models/CartItems.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Models
+{
+    public class CartItems
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+
+        //PoCo=Plain CLR Object. Class that doesn't inhearit from anything or implement anything
+    }
+}
